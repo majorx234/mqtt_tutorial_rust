34c2433ab37c1d32917fa1193466b448c9634b4c
Majorx234 <majorx234@googlemail.com>
2024-08-08 16:16:01 +0200

    Add sending part to complete example

--- src/bin/mqtt_publisher/main.rs
+++ src/bin/mqtt_publisher/main.rs
@@ -1,8 +1,7 @@
 use std::process;
+use std::{thread, time};
 
 extern crate paho_mqtt as mqtt;
-
-const QOS: i32 = 1;
 
 fn main() {
     println!("WIP mqtt publisher!");
@@ -14,7 +13,7 @@ fn main() {
         .client_id("mqtt_publisher_rust")
         .finalize();
 
-    let cli = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
+    let mqtt_client = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
         println!("Error creating the client: {:?}", e);
         process::exit(1);
     });
@@ -22,10 +21,24 @@ fn main() {
     let conn_opts = mqtt::ConnectOptions::new();
 
     // Connect and wait for it to complete or fail
-    if let Err(err) = cli.connect(conn_opts).wait() {
+    if let Err(err) = mqtt_client.connect(conn_opts).wait() {
         println!("Unable to connect: {}", err);
         process::exit(1);
     }
+    let topic = mqtt::Topic::new(&mqtt_client, "testing", mqtt::QOS_1);
 
-    let msg = mqtt::Message::new("test", "Hello world!", QOS);
+    let mut value = 0;
+    for _ in 0..10 {
+        let msg = format!("{{\"value\": \"{}\"}}", value);
+        let msg_token = topic.publish(msg);
+        if let Err(e) = msg_token.wait() {
+            println!("Error sending message: {:?}", e);
+            break;
+        }
+        thread::sleep(time::Duration::from_millis(1000));
+        value += 1;
+    }
+
+    let tok = mqtt_client.disconnect(None);
+    tok.wait().unwrap();
 }
